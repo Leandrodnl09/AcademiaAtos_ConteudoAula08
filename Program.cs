ba74dc8f44e9e4ba95e5d9478418e88b933430f4
Leandro Di Nardo Lazarin <leandrodnl09@gmail.com>
2023-04-13 19:05:45 -0300

    ConteudoAula

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace AcademiaAtos_ConteudoAula
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Aula 08 -> Vetores;
+
+            int[] vetor = new int[3]; // declaração de vetor;
+
+            int[] vetor1 = new int[] { 1, 2, 3};
+
+            Console.WriteLine("Digite um valor do tamnanho do vetor: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] vetorN = new int[n]; // tamanho do vetor definido pelo usuario.
+
+
+            // percorrer dentro do vetor
+            for (int i = 0; i < vetorN.Length; i++) // boas praticas
+            {
+                Console.WriteLine($"Informe um valor para a posição {i + 1}: {i} ");
+                vetorN[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < vetorN.Length; i++) // boas praticas
+            {
+                Console.WriteLine($"Os valores digitatos é: {vetor[i]} "); // escreve o valor de todas as posições do vetor;
+            }
+
+
+        }
+    }
+}
